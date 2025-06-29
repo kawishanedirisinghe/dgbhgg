9e86bf914fe42f5f034c0b2df5ce04367092a5a3
kawishanedirisinghe <kawishanedirisinghe@gmail.com>
2025-06-29 20:29:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -568,7 +568,7 @@ function handleUpdate($update) {
                 CURLOPT_POSTFIELDS => [
                     'chat_id' => $chatId,
                     'document' => new CURLFile(realpath($tempFileName), 'text/plain', 'response.txt'),
-                    'caption' => 'Response too fucking long, here’s the full shit as a file. Or View fucking Url Use' . basename($tempFileName)
+                    'caption' => 'Response too fucking long, here’s the full shit as a file. Or View fucking Url Usehttps://catseek.onrender.com/tmp/' . basename($tempFileName)
                 ],
                 CURLOPT_HTTPHEADER => [
                     "Content-Type: multipart/form-data"
